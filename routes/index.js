a7fe4fb7780e0158b7090a4c192aa6c6f5528fc2
Peter <pqiu85674@gmail.com>
2024-06-19 18:48:59 +0800

    signup signin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+let Users = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  // res.render("index", { title: "Express" });
+  res.send({
+    status: "true",
+    message: "message",
+    Users: Users
+  });
+});
+//POST 動作
+router.post("/signup", function(req, res) {
+  if (Users.filter(item => item.email === req.body.email).length === 0) {
+    Users.push({
+      email: req.body.email,
+      password: req.body.password
+    });
+    res.send({
+      status: true,
+      message: "成功",
+      Users: Users
+    });
+  } else {
+    res.send({
+      status: false,
+      message: "已經有相同帳號"
+    });
+  }
 });
 
+router.post("/signin", function(req, res) {
+  if (
+    Users.filter(
+      item =>
+        item.email === req.body.email && item.password === req.body.password
+    ).length === 1
+  ) {
+    res.send({
+      status: "true",
+      message: `哈囉 ${req.body.email} ，成功登入`
+    });
+  } else {
+    res.send({
+      status: "false",
+      message: "無法登入"
+    });
+  }
+});
+
+router.use(function(req, res) {
+  res.status(404).send("網址錯誤");
+});
 module.exports = router;
